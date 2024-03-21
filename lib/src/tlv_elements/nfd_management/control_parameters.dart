979392db62b5d0befade713027dee77212cb255d
Jan Romann <jan.romann@hs-emden-leer.de>
2024-03-21 18:13:05 +0100

    fixup! feat: improve NFD management protocol implementation

--- lib/src/tlv_elements/nfd_management/control_parameters.dart
+++ lib/src/tlv_elements/nfd_management/control_parameters.dart
@@ -5,15 +5,34 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 import "../../extensions/bytes_encoding.dart";
+import "../../extensions/non_negative_integer.dart";
 import "../../result/result.dart";
 import "../name/name.dart";
 import "../name/name_component.dart";
 import "../tlv_element.dart";
 import "../tlv_type.dart";
+import "tlv_elements/expiration_period.dart";
+import "tlv_elements/face_id.dart";
+import "tlv_elements/flags.dart";
+import "tlv_elements/origin.dart";
 
 final class ControlParameters extends KnownTlvElement {
   const ControlParameters({
     this.name,
+    this.faceId,
+    this.uri,
+    this.localUri,
+    this.origin,
+    this.cost,
+    this.capacity,
+    this.count,
+    this.baseCongestionMarkingInterval,
+    this.defaultCongestionThreshold,
+    this.mtu,
+    this.flags,
+    this.mask,
+    this.strategy,
+    this.expirationPeriod,
   });
 
   static Result<ControlParameters> fromValue(List<int> value) {
@@ -25,6 +44,34 @@ final class ControlParameters extends KnownTlvElement {
 
   final Name? name;
 
+  final NonNegativeInteger? faceId;
+
+  final Uri? uri;
+
+  final Uri? localUri;
+
+  final NonNegativeInteger? origin;
+
+  final NonNegativeInteger? cost;
+
+  final NonNegativeInteger? capacity;
+
+  final NonNegativeInteger? count;
+
+  final NonNegativeInteger? baseCongestionMarkingInterval;
+
+  final NonNegativeInteger? defaultCongestionThreshold;
+
+  final NonNegativeInteger? mtu;
+
+  final NonNegativeInteger? flags;
+
+  final NonNegativeInteger? mask;
+
+  final Name? strategy;
+
+  final NonNegativeInteger? expirationPeriod;
+
   @override
   TlvType get tlvType => TlvType.controlParameters;
 
@@ -35,6 +82,31 @@ final class ControlParameters extends KnownTlvElement {
     final name = this.name;
     if (name != null) {
       encodedValue.addAll(name.encode());
+    }
+
+    final faceId = this.faceId;
+    if (faceId != null) {
+      encodedValue.addAll(FaceId(faceId).encode());
+    }
+
+    final origin = this.origin;
+    if (origin != null) {
+      encodedValue.addAll(Origin(origin).encode());
+    }
+
+    final cost = this.cost;
+    if (cost != null) {
+      encodedValue.addAll(Origin(cost).encode());
+    }
+
+    final flags = this.flags;
+    if (flags != null) {
+      encodedValue.addAll(Flags(flags).encode());
+    }
+
+    final expirationPeriod = this.expirationPeriod;
+    if (expirationPeriod != null) {
+      encodedValue.addAll(ExpirationPeriod(expirationPeriod).encode());
     }
 
     return encodedValue;
